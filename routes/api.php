5bb374b276f51d67f3181e96e9759f4d1a4bde50
Emanuele Speca <e.speca@izs.it>
2023-04-14 15:16:02 +0200

    api con crud

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+*/
+
+Route::get('/posts',function(){
+return Post::all();
+});
+Route::get('/posts/{id}',function($id){
+
+    return Post::findOrFail($id);
+    });
+Route::post('/posts/create',function(Request $request){   
+$post = new Post;
+$post->titolo =  $request->input('titolo');
+$post->contenuto =  $request->input('contenuto');
+$post->user_id =1;
+$post->save();
+return "Post saved";
+});
+Route::patch('/posts/{anno}/{mese}/{giorno}/{id}',function(Request $request,$id){
+$post = Post::findOrFail($id);
+$post->titolo =  $request->input('titolo');
+$post->contenuto =  $request->input('contenuto');
+$post->save();
+});
+Route::delete('/posts/{id}', function($id){
+$id = Post::findOrFail($id);
+$id->delete();
 });
